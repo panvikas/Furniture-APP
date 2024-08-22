5d02d86a48699ca1eda6d0488ede5e925698f6a0
panvikas <54739224+panvikas@users.noreply.github.com>
2024-08-22 04:18:35 -0700

    Add files via upload

---
+++ Furniture-Shop-App-UI-SwiftUI-main/Furniture_app/Furniture_appApp.swift
@@ -0,0 +1,16 @@
+//
+//  Furniture_appApp.swift
+//  Furniture_app
+//
+
+
+import SwiftUI
+
+@main
+struct Furniture_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
